5204b85f103ed53e0ffb3c3b1ff8ba5fcc69f10c
Jeremiah Flaga <j.flaga@arcanys.com>
2020-03-06 14:03:03 +0800

    example of generating SQL script from CSV file

---
+++ CSharpPlayground/CSharpPlayground/2020-03-06-generate-sql-update-from-csv/README.md
@@ -0,0 +1,8 @@
+﻿# Generate Update SQL script from data in csv file
+
+``` sql
+UPDATE Table
+SET col2 = 'col value'
+WHERE col1 = 'col_1_val';
+GO
+```
---
+++ CSharpPlayground/CSharpPlayground/2020-03-06-generate-sql-update-from-csv/SQLScriptGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace CSharpPlayground._2020_03_06_generate_sql_update_from_csv
+{
+    public class SQLScriptGeneratorTests
+    {
+        [Fact]
+        public void generate_sql_script()
+        {
+            // https://stackoverflow.com/a/5283044
+            string sqlScript = "";
+            using (var reader = new StreamReader("2020-03-06-generate-sql-update-from-csv/data.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var indexOfFirstComma = line.IndexOf(',');
+                    var code = line.Substring(0, indexOfFirstComma).Replace("'", "''");
+                    var theRest = line.Substring(indexOfFirstComma + 2).Replace("'", "''");
+
+                    sqlScript += CreateSQLScript(code, theRest);
+                }
+            }
+
+            Assert.NotEmpty(sqlScript);
+        }
+
+        private string CreateSQLScript(string code, string theRest)
+        {
+            return $@"
+                UPDATE Table
+                SET Column1 = '{theRest}'
+                WHERE Column2 = '{code}';
+                GO
+            ";
+        }
+    }
+}
